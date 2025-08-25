5a6790cf0ce1940c682f9497350eab001c5781a1
ahmedgoher <agoher190@gmail.com>
2025-08-25 15:04:41 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,26 +2,28 @@
 const darkBtn = document.getElementById("darkModeBtn");
 const body = document.body;
 const themeKey = "theme";
-const darkTheme = "dark";
+const darkTheme = "dark-mode";
 const lightTheme = "light";
 
 // Set initial theme on page load
 const currentTheme = localStorage.getItem(themeKey) || lightTheme;
 if (currentTheme === darkTheme) {
-  body.classList.add("dark-mode");
-  darkBtn.textContent = "Light Mode";
+  body.classList.add(darkTheme);
+  darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i>'; // Light Mode Icon
 } else {
-  body.classList.remove("dark-mode");
-  darkBtn.textContent = "Dark Mode";
+  body.classList.remove(darkTheme);
+  darkBtn.innerHTML = '<i class="fa-solid fa-moon"></i>'; // Dark Mode Icon
 }
 
 // Toggle theme on button click
 darkBtn.addEventListener("click", () => {
-  if (body.classList.toggle("dark-mode")) {
-    darkBtn.textContent = "Light Mode";
+  if (body.classList.toggle(darkTheme)) {
+    darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
     localStorage.setItem(themeKey, darkTheme);
   } else {
-    darkBtn.textContent = "Dark Mode";
+    darkBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
     localStorage.setItem(themeKey, lightTheme);
   }
 });
+
+document.getElementById("year").textContent = new Date().getFullYear();
